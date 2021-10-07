c3c3855e4605347ea0ff71657d9feda6778e1e34
Matheus Maia Ávila <65270822+matheus97eng@users.noreply.github.com>
2021-10-07 17:53:51 -0300

    Update grid.py

--- bfs/grid.py
+++ bfs/grid.py
@@ -1,20 +1,45 @@
 import sys
 
 def check(y,x,n,m):
+  '''
+  checa se as coordenadas x e y estão dentro do grid
+  
+  input: 
+    y - coordenada y
+    x - coordenada x
+    n - limite de linhas
+    m - limite de colunas
+   
+   retorna o valor lógico da operação
+  '''
   return (y >= 0 and y < n and x >= 0 and x < m)
 
 def test(n, m):
+  '''
+  função que realiza o teste do problema.
+  Aqui usamos uma lista python para simular uma fila. A cada iteração, chamamos o próximo elemento da lista, que é como se estivessemos excluindo o primeiro elemento
+  de uma fila. O objeto fila é portanto uma lista, mas que funciona como uma fila.
+  Para simular uma matriz, foi usado lista de listas. Por exemplo, o objeto mark é uma lista de listas que funciona como matriz. Ao acessarmos mark[l][c] estamos acessando
+  na verdade o c-ésimo elemento da l-ésima lista, mas podemos pensar que estamos acessando o elemento de linha l e coluna c.
+  
+  input:
+    n - número de linhas
+    m - número de colunas
+    
+  return:
+    resposta do teste. -1 caso não seja possível se chegar ao final do grid. Caso seja possível, retorna o número de passos dados
+  '''
 
   # iniciando variáveis
-  mark = [[-1]*m for i in range(n)]
-  movs = {'y':[1,-1,0,0], 'x':[0,0,1,-1]}
-  fila = []
+  mark = [[-1]*m for i in range(n)]         # matriz que indica quais posições foram visitadas e quantos passos foram dados para chegar nela
+  movs = {'y':[1,-1,0,0], 'x':[0,0,1,-1]}   # dicionário usado para realizar os movimentos
+  fila = []                                 # lista que funciona como fila
 
   # começando o programa
   fila.append((0,0))  # cada elemento da fila é uma tupla com as coordenadas
   mark[0][0] = 0    # começamos com o passo de número 0 no canto superior esquerdo
-  sz = 1
-  pos = 0
+  sz = 1            # tamanho atual da fila
+  pos = 0           # posição atual da fila
 
   while (pos!=sz):
     x = fila[pos][1]
